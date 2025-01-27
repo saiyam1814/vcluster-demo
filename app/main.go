dbb380a97ebe8765f183e3c5af99d7e365acc2e8
Saiyam Pathak <saiyam911@gmail.com>
2025-01-27 16:54:21 +0530

    Update main.go

--- app/main.go
+++ app/main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Starting server on :8080")
+	fmt.Println("Startting server on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
